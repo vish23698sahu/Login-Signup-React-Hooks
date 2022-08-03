f746cd612f91e593b630220c37b794b32c1dbeef
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-08-03 19:38:06 +0530

    version - 5 Dummy data removed Data fetch from Firebase

--- src/App.js
+++ src/App.js
@@ -2,39 +2,10 @@ import { Fragment, useState } from 'react';
 import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 
-const DUMMY_USERS = [
-  {
-    id: 'e1',
-    name: 'Vishakha',
-    lastName: 'Sahu',
-    email: 'vish@gmail.com',
-    pass: 'vish'
-  },
-  {
-    id: 'e2',
-    name: 'Kiran',
-    lastName: 'Sahu',
-    email: 'kiran@gmail.com',
-    pass: 'kiran'
-  },
-  {
-    id: 'e3',
-    name: 'Mohit',
-    lastName: 'Sharma',
-    email: 'mohit@gmail.com',
-    pass: 'mohit'
-  }
-];
-
 function App() {
-  const [dummyUsers, setDummyUsers] = useState(DUMMY_USERS);
   const [isUserRegistered, setIsUserRegistered] = useState(false);
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
   const [showLogin, setShowLogin] = useState(true);
-
-  const loginHandler = () => {
-    setIsUserLoggedIn(true);
-  }
 
   const signUpInsideLoginHandler = () => {
     setIsUserRegistered(true);
@@ -47,9 +18,6 @@ function App() {
   }
 
   const userRegisteredHandler = async (newUserData) => {
-    // setDummyUsers((prevusers) => {
-    //   return [newUserData, ...prevusers];
-    // })
     await fetch('https://login-signup-portfolio-default-rtdb.firebaseio.com/Users.json', {
       method: 'POST',
       body: JSON.stringify({
@@ -60,7 +28,7 @@ function App() {
 
   return (
     <Fragment>
-      {showLogin && <Login onSignUp={signUpInsideLoginHandler} userLoggedIn={isUserLoggedIn} users={dummyUsers} />}
+      {showLogin && <Login onSignUp={signUpInsideLoginHandler} userLoggedIn={isUserLoggedIn} />}
       {!showLogin && <SignUp userSignedUp={isUserRegistered} onLoginInsideRegister={loginInsideRegister} onUserRegistration={userRegisteredHandler} />}
     </Fragment>
   );
